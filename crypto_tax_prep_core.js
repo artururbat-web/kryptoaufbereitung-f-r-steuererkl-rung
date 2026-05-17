c5e3df84817f3fa60a0158e5d2fd3461d5442a47
artururbat-web <artur.urbat@gmail.com>
2026-05-17 18:29:08 +0200

    Add rebate type to transaction types

--- crypto_tax_prep_core.js
+++ crypto_tax_prep_core.js
@@ -140,6 +140,7 @@
       sell: "SELL",
       staking: "INCOME",
       fixed_staking: "INCOME",
+      rebate: "INCOME",
       deposit: "TRANSFER_IN",
       withdrawal: "TRANSFER_OUT",
       withdraw: "TRANSFER_OUT",
